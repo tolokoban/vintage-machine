f2572dd87016c0797485a681b79b731ba6955be1
Tolokoban <contact@tolokoban.org>
2016-10-04 17:17:52 +0200

    Another bad commit. Still white...

--- src/mod/app.js
+++ src/mod/app.js
@@ -2,17 +2,23 @@
 
 var Keyboard = require("keyboard");
 var Kernel = require("kernel");
-var Asm = require("asm");
+
 /*
+var Asm = require("asm");
 var asm = new Asm( null, [[1,2], [3,4], Asm.ADD] );
 debugger;
 asm.next();
-*/
+ */
+
 exports.start = function() {
     var img = new Image();
     img.src = "css/app/symbols.jpg";
     img.onload = function() {
         var canvas = document.getElementById( 'CANVAS' );
         var kernel = new Kernel( canvas, img );
+    };
+    img.onerror = function( err ) {
+        console.error( err );
+        console.error("Unable to load image `" + img.src + "`!");
     };
 };
